55b740f61198150e3dade5b5d23880874dff464e
OlavOlsm <OlavOlsm@gmail.com>
2015-10-06 13:13:13 +0200

    ConnectToDB implements AutoCloseable

--- ConnectToDB.java
+++ ConnectToDB.java
@@ -4,7 +4,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class ConnectToDB {
+public class ConnectToDB implements AutoCloseable {
 	private Connection con;
 	
 	// Connect to database using DriverManager.getConnection
