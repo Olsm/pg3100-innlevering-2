679c308147c3d071b22863d65123b84659ded4dd
OlavOlsm <OlavOlsm@gmail.com>
2015-10-06 13:06:48 +0200

    Constructor created

--- DBHandlerBokliste.java
+++ DBHandlerBokliste.java
@@ -18,21 +18,43 @@ public class DBHandlerBokliste {
 	private PreparedStatement pstmtGetTable;
 	private PreparedStatement pstmtGetRow;
 	
-	public DBHandlerBokliste (String user, String password) throws SQLException {}
+	public DBHandlerBokliste (String user, String password) throws SQLException {
+		tableName = "pg3100";
+		
+		// Connect to database localhost and get connection.
+		db = new ConnectToDB ("localhost", tableName, user, password);
+		con = db.getConnection();
+	}
 	
-	public void close() throws SQLException {}
+	public void close() throws SQLException {
+		
+	}
 	
-	public int updateTittel (String nyTittel, String tittel) throws SQLException {}
+	public int updateTittel (String nyTittel, String tittel) throws SQLException {
+		return 0;
+		}
 	
-	public int UpdateForfatter (String nyForfatter, String forfatter) throws SQLException {}
+	public int UpdateForfatter (String nyForfatter, String forfatter) throws SQLException {
+		return 0;
+		}
 	
-	public int deleteForfatter (String forfatter) throws SQLException {}
+	public int deleteForfatter (String forfatter) throws SQLException {
+		return 0;
+		}
 	
-	public int deleteTittel (String tittel) throws SQLException {}
+	public int deleteTittel (String tittel) throws SQLException {
+		return 0;
+		}
 	
-	public int insertRow (String isbn, String forfatter, String tittel) throws SQLException {}
+	public int insertRow (String isbn, String forfatter, String tittel) throws SQLException {
+		return 0;
+		}
 	
-	public ArrayList<String> getTable() throws SQLException {}
+	public ArrayList<String> getTable() throws SQLException {
+		return null;
+		}
 	
-	public String getRow (String forfatter, String tittel) throws SQLException {}
+	public String getRow (String forfatter, String tittel) throws SQLException {
+		return tittel;
+		}
 }
