1f0d93f37fb885ab7b18d01e67c01e1de785499b
OlavOlsm <OlavOlsm@gmail.com>
2015-10-06 12:24:33 +0200

    ConnectToDB class created

--- ConnectToDB.java
+++ ConnectToDB.java
@@ -1,14 +1,26 @@
 package innlevering2;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConnectToDB {
 	private Connection con;
 	
-	public ConnectToDB (String serverName, String databaseName, String user, String password) throws SQLException {}
+	// Connect to database using DriverManager.getConnection
+	public ConnectToDB (String serverName, String databaseName, 
+			String user, String password) throws SQLException {
+		con = DriverManager.getConnection("jdbc:mysql://" + 
+			serverName + "/" + databaseName, user, password);
+	}
 	
-	public void close() throws SQLException {}
+	// Close the database connection
+	public void close() throws SQLException {
+		con.close();
+	}
 	
-	public Connection getConnection() {}
+	// Return the database connection
+	public Connection getConnection() {
+		return con;
+	}
 }
